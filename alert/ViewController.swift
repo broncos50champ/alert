6e02ac7516e11fa09069d9b86e3e7be22b559f61
Luis Segovia <luiissegovia@icloud.com>
2022-03-28 14:21:40 -0600

    mvp

--- alert/ViewController.swift
+++ alert/ViewController.swift
@@ -14,6 +14,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: Action Functions and Other Functions go here.
+    @IBAction func AlertButton(_ sender: Any) {
+        let alert = UIAlertController(title: "What is your name?", message: nil, preferredStyle: .alert)
+        self.present(alert, animated: true)
+        
+        let ok = UIAlertAction(title: "OK",style: .default){
+            
+            action in
+            
+            print("OK Pressed")
+            
+        }
+        
+        alert.addAction(ok)
+    
+        let cancel = UIAlertAction (title: "cancel", style: .cancel)
+        
+        alert.addAction(cancel)
+    }
 }
+
+
+
+
+    
+
+
+
+
+
+
 
